fc9542677b4272b3e71ad02ea1a0cb2f7dd0b711
elishaoigara <elishaoigara50@gmail.com>
2025-01-12 06:57:47 -0800

    taskadvjs

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
+const { list } = require('./100-data.js'); // Import the list from the 100-data.js file
 
-const newList = list.map((val, idx) => val * idx);
+// Create a new list where each element is multiplied by its index
+const newList = list.map((value, index) => value * index);
+
+// Print both the original and new lists
 console.log(list);
 console.log(newList);
+
